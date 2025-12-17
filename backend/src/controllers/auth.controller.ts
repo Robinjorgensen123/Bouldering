68b217c447b283387c2c8c63edff66b4c79a0113
robjor88 <robjor88@gmail.com>
2025-12-17 20:00:01 +0100

    userSchema & UserModel, UserController

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { User } from "../models/User.js";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({
+        message: "E-postadressen används redan",
+      });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      email,
+      passwordHash: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Användare skapad",
+    });
+  } catch (error) {
+    console.error("Registreringsfel:", error);
+    res.status(500).json({
+      message: "Serverfel",
+    });
+  }
+};
---
+++ backend/src/models/User.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  passwordHash: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      requred: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = model<IUser>("User", userSchema);
