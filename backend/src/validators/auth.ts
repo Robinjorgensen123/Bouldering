5b96d81be0559efb73c7b9666896be9413e5b03e
robjor88 <robjor88@gmail.com>
2025-12-17 19:44:02 +0100

    register och login Schema Joi

--- backend/src/validators/auth.ts
+++ backend/src/validators/auth.ts
@@ -1,9 +1,16 @@
 import Joi from "joi";
-
+//Registrera användare
 export const registerSchema = Joi.object({
   email: Joi.string().email().required().messages({
-    'string.email' : 'Måste vara en giltig e-postadress',
-    'any.required': 'E-post är obligatorisk'
+    "string.email": "Måste vara en giltig e-postadress",
+    "any.required": "E-post är obligatorisk",
   }),
-  password: 
+  password: Joi.string().min(6).required().messages({
+    "string.min": "Lösenordet måste vara minst 6 tecken",
+  }),
+});
+// Logga in användare
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
 });
