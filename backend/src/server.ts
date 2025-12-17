657b0a40979604bb7fc0bfab68d0ff39b93c9858
robjor88 <robjor88@gmail.com>
2025-12-17 21:40:15 +0100

    api routes för login och register user

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/api/health", (_req, res) => {
   res.json({ status: "OK" });
