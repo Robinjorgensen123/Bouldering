2804d85bc601606ea4e5436f5ad6b696f517281f
robjor88 <robjor88@gmail.com>
2025-12-18 12:09:03 +0100

    MongoDB connected

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
-
+connectDB();
 const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
